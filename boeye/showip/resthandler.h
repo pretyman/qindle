f3b650848e66935aca43d58ee7414b6ff2262411
lm3783@gmail.com <lm3783@gmail.com@bc56459a-a753-11de-afd4-1d14fbec496b>
2014-02-20 10:17:17 +0000

    update resthandler

--- boeye/showip/resthandler.h
+++ boeye/showip/resthandler.h
@@ -2,17 +2,34 @@
 #define RESTHANDLER_H
 
 #include <QObject>
+#include <QSettings>
+#include <QUrl>
+#include <QNetworkReply>
+#include <QNetworkAccessManager>
+#include <QDir>
 
 class resthandler : public QObject
 {
     Q_OBJECT
 public:
     explicit resthandler(QObject *parent = 0);
+    ~resthandler();
+    int getToken();
+    QUrl LoginPage();
+    void getFileList(QDir path);
 
 signals:
+    void LoginSuccess();
+    void LoginFailure();
 
 public slots:
+    void getURL(QUrl url);
+    void LoginReplyFinished(QNetworkReply *reply);
+    void ListReplyFinished(QNetworkReply *reply);
 
+private:
+    QSettings *settings;
+    QNetworkAccessManager *manager;
 };
 
 #endif // RESTHANDLER_H
