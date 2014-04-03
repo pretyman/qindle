357e61e522249de32ce36dc86129b79377b8cfe3
lm3783@gmail.com <lm3783@gmail.com@bc56459a-a753-11de-afd4-1d14fbec496b>
2014-04-03 05:43:04 +0000

    add new class

--- trunk/boeye/showip/webfilewidget.h
+++ trunk/boeye/showip/webfilewidget.h
@@ -2,16 +2,32 @@
 #define WEBFILEWIDGET_H
 
 #include <QTreeWidget>
+#include <QPaintEvent>
+#include <QRect>
+#include "resthandler.h"
 
 class WebFileWidget : public QTreeWidget
 {
     Q_OBJECT
 public:
     explicit WebFileWidget(QWidget *parent = 0);
+    void connectHandler(resthandler *handler);
 
 signals:
+    void getFolderContent(QDir path);
+    void UpdateWindow(QRect region);
+
+protected:
+//    void paintEvent(QPaintEvent *event);
 
 public slots:
+    void setFolderContent(QList<QString> *list);
+    void itemExpand(QTreeWidgetItem *item);
+    void itemClick(QTreeWidgetItem * item, int);
+    void itemDownload(QTreeWidgetItem *item);
+private:
+    resthandler *rhandler;
+    void itemCheckState(QTreeWidgetItem *item, Qt::CheckState state);
 
 };
 
